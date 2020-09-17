f8a265b48d804e4656a69f9d5b204611543c7b56
谷政烨 <18534132836@126.com>
2020-09-17 17:34:21 +0800

    test

---
+++ demo-1/src/test/java/com/example/demo/testDateTime/TestDateTime.java
@@ -0,0 +1,40 @@
+package com.example.demo.testDateTime;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+public class TestDateTime {
+	
+	
+	@Test
+	public void testDate() {
+		//字符串和日期之间的转换
+		String strDay = "20190801";
+		DateTimeFormatter dtf =  DateTimeFormatter.ofPattern("yyyyMMdd");
+		//字符串转换为日期
+		LocalDate date = LocalDate.parse(strDay, dtf);
+		System.out.println(date.toString());
+		System.out.println(date.getMonthValue());
+		System.out.println(date.getDayOfWeek());
+		System.out.println(date.getDayOfYear());
+		//日期转换为字符
+		System.out.println(date.format(dtf));
+	}
+	
+	@Test
+	public void testCaculator() {
+		int nubOne = 1;
+		int nubTwo = 3;
+		String operate = "+";
+		String strResult = "";
+		String strOperate = "+-*/";
+		if(strOperate.contains(operate.trim())) {
+			strResult = nubOne + operate.trim()+ nubTwo;
+		}
+		System.out.println(strResult);
+	}
+
+}
--- demo-1/src/test/java/com/example/demo/testdata/MySQLConnectionTest.java
+++ demo-1/src/test/java/com/example/demo/testdata/MySQLConnectionTest.java
@@ -4,7 +4,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 
 
-@SpringBootTest()
+@SpringBootTest
 public class MySQLConnectionTest {
 	
 	
