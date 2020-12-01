376b623d0c0e8aa8fd824af939ba2d6064955d0d
谷政烨 <18534132836@126.com>
2020-12-01 09:40:48 +0800

    2020年12月1日09:40:26 test difference

--- demo-2/src/test/java/com/example/demo/huaweicode/TestGitCommit.java
+++ demo-2/src/test/java/com/example/demo/huaweicode/TestGitCommit.java
@@ -1,8 +1,15 @@
 package com.example.demo.huaweicode;
 
+import org.junit.jupiter.api.Test;
+
 public class TestGitCommit {
 
+    @Test
     public  void test(){
         System.out.println("x");
+    }
+    @Test
+    public  void  testCommit(){
+        System.out.println(1);
     }
 }
